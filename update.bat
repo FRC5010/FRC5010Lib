45ce65c70505e521d2dc59a22f7338af0356f8ea
Curt <clrozeboom@gmail.com>
2024-05-07 21:13:08 -0400

    Try different layout

--- update.bat
+++ update.bat
@@ -4,4 +4,4 @@ call gradlew.bat :spotlessApply
 echo Publish
 call gradlew.bat publish
 echo Copy
-Xcopy /E /y .\\build\\repos\\releases\\org\\frc5010 .\\frc5010lib\\repos\\frc5010lib
+Xcopy /E /y .\\build\\repos\\releases\\org\\frc5010 .\\frc5010lib\\repos\\org\\frc5010
