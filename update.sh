35e9c036d0c76df41ed13c4deadecefe4deec730
Curt <clrozeboom@gmail.com>
2024-08-21 21:52:36 -0400

    Many comments added

--- update.sh
+++ update.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 ./gradlew :spotlessApply
+dos2unix build.gradle
+dos2unix publish.gradle
+dos2unix config.gradle
+dos2unix README.md
 ./gradlew publish
 cp -rf build/repos/releases/org/frc5010 frc5010lib/repos/
