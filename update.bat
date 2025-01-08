66a613a9bb1c3082c453e0d4e7d3c77a2100060a
Curt <clrozeboom@gmail.com>
2025-01-07 21:19:53 -0500

    Try something else

--- update.bat
+++ update.bat
@@ -3,5 +3,5 @@ echo Formatting
 call gradlew.bat :spotlessApply
 echo Publish
 call gradlew.bat publish
-echo Copy
-Xcopy /E /y .\\build\\repos\\releases\\org\\frc5010 .\\frc5010lib\\repos\\org\\frc5010
+@REM echo Copy
+@REM Xcopy /E /y .\\build\\repos\\releases\\org\\frc5010 .\\frc5010lib\\repos\\org\\frc5010
