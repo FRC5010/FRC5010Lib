28bdebcc3f9f65f23eb37165a8fc85436b209431
Curt <clrozeboom@gmail.com>
2025-01-07 20:17:38 -0500

    Try changing update.sh

--- update.sh
+++ update.sh
@@ -5,4 +5,4 @@ dos2unix publish.gradle
 dos2unix config.gradle
 dos2unix README.md
 ./gradlew publish
-cp -rf build/repos/releases/org/frc5010 frc5010lib/repos/
+cp -rf build/repos/releases/ frc5010lib/repos/
